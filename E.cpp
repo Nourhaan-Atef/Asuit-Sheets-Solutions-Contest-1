c1ccb1adf00c2c38955200daf6b99dcaed670534
Nourhan-Atef-Hossam-El-Deen <91796430+Nourhaan-Atef@users.noreply.github.com>
2022-07-22 12:55:53 +0200

    E Problem

---
+++ E.cpp
@@ -0,0 +1,12 @@
+	#include <iostream>
+	#include <cmath>
+	using namespace std;
+	int main(){
+	int a,b;
+	cin>>a>>b;
+	if((a==0&&b==0)||(abs(a-b)>=2)){
+		cout<<"NO"<<endl;
+	}else{
+		cout<<"YES"<<endl;
+	}
+	}
